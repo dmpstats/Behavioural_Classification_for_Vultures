5fe5ffeab12cc163bd04dd0a74d8410b26f85ab5
Lindesay Scott-Hayward <lass@st-and.ac.uk>
2024-03-20 17:13:26 +0000

    update testing code
    edit testing code to look at bad speed time cases

--- tests/app-testing.r
+++ tests/app-testing.r
@@ -18,8 +18,8 @@ test_dt$metadata
 
 
 # thinning gaia and nam dataset for 5mins gap for faster testing. 
-test_dt$gaia <- mt_filter_per_interval(test_dt$gaia, unit = "2 min")
-test_dt$nam_sop <- mt_filter_per_interval(test_dt$nam_sop, unit = "2 min")
+test_dt$gaia <- mt_filter_per_interval(test_dt$gaia, unit = "5 min")
+test_dt$nam_sop <- mt_filter_per_interval(test_dt$nam_sop, unit = "5 min")
 
 
 # ---------------------------------------- #
@@ -199,5 +199,177 @@ track_dt |> filter(track == "AW196499..deploy_id.2023814814.") |>
 
 
 
+# nam_sop: "GA_6594" (runSALSA1D error)
+# savanha: "SAV.4360.A" (runSALSA1D error)
+# ken_tnz: "C175593" (runSALSA1D error); "A234556" (non-convergence)
+# wcs: "AW196499..deploy_id.2023814814.", "B175706..deploy_id.2023814800." (non-convergence)
+# wb_zam_knd: "A234556..deploy_id.2153340562." (non-convergence); "X163116Z..deploy_id.2388610874" (runSALSA1D error)
+
+lindesay_dt <- test_dt[!names(test_dt) %in% c("metadata", "gaia", "pan_afr", "sa_vfa")]
+lindesay_dt$nam_sop <- lindesay_dt$nam_sop |> filter(track == "GA_6594")
+lindesay_dt$savahn <- lindesay_dt$savahn |> filter(track == "SAV.4360.A")
+lindesay_dt$ken_tnz <- lindesay_dt$ken_tnz |> filter(track %in% c("C175593", "A234556"))
+lindesay_dt$wcs <- lindesay_dt$wcs |> filter(track %in% c("AW196499..deploy_id.2023814814.", "B175706..deploy_id.2023814800."))
+#lindesay_dt$wb_zam_knd <- lindesay_dt$wb_zam_knd |> filter(track %in% c("A234556..deploy_id.2153340562.", "X163116Z..deploy_id.2388610874"))
+
+# use this to reload Rfunction with browser
+set_interactive_app_testing()
+# ~~~~~~~~~~~~~~~~~~
+# TANZANIA
+# ~~~~~~~~~~~~~~~~~~
+track_dt <- rFunction(
+  data = lindesay_dt$ken_tnz,
+  travelcut = 3,
+  create_plots = TRUE,
+  sunrise_leeway = 0,
+  sunset_leeway = 0,
+  altbound = 25,
+  keepAllCols = FALSE
+)
+
+# trying a slightly different time range...
+track_dt_sub <- rFunction(
+  data = filter(lindesay_dt$ken_tnz, timestamp < max(timestamp)-days(20)),
+  travelcut = 3,
+  create_plots = TRUE,
+  sunrise_leeway = 0,
+  sunset_leeway = 0,
+  altbound = 25,
+  keepAllCols = FALSE
+)
+
+mod_out <- speed_time_model(track_dt |> filter(track == "C175593"), model_obj = TRUE, void_non_converging = TRUE)
+# salsa error if use full data
+# change to gaussian with log link and fits fine, however, mean var is not good as variance increases with mean....
+
+mod_out <- speed_time_model(track_dt_sub |> filter(track == "C175593"), model_obj = TRUE, void_non_converging = TRUE)
+# model not converged but not classed as NULL by p-values
+# no values outside CIs so no changes
+# seems like a fairly immobile bird
+# change to gaussian "fixes"issues
+
+# 
+
+mod_out <- speed_time_model(track_dt |> filter(track == "A234556"), model_obj = TRUE, void_non_converging = TRUE)
+# not converged, fit changed to NULL
+# gaussian works but knot really close to left boundary, 
+# changed to have gap=0.5 --> half an hour between knots
+
+mod_out <- speed_time_model(track_dt_sub |> filter(track == "A234556"), model_obj = TRUE, void_non_converging = TRUE)
+# gaussian works but very uncertain in last part (13hrs post sunrise). not really an issue
 
 
+
+# ~~~~~~~~~~~~~~~~~~
+# WCS
+# ~~~~~~~~~~~~~~~~~~
+# wcs: "AW196499..deploy_id.2023814814.", "B175706..deploy_id.2023814800." (non-convergence)
+set_interactive_app_testing()
+
+track_dt <- rFunction(
+  data = lindesay_dt$wcs,
+  travelcut = 3,
+  create_plots = TRUE,
+  sunrise_leeway = 0,
+  sunset_leeway = 0,
+  altbound = 25,
+  keepAllCols = FALSE
+)
+
+# trying a slightly different time range...
+track_dt_sub <- rFunction(
+  data = filter(lindesay_dt$wcs, timestamp < max(timestamp)-days(20)),
+  travelcut = 3,
+  create_plots = TRUE,
+  sunrise_leeway = 0,
+  sunset_leeway = 0,
+  altbound = 25,
+  keepAllCols = FALSE
+)
+
+mod_out <- speed_time_model(track_dt |> filter(track == "AW196499..deploy_id.2023814814."), model_obj = TRUE, void_non_converging = TRUE)
+# model not converged with gamma
+# change to gaussian with log link and fits fine, however, mean var is not good as variance increases with mean....
+
+mod_out <- speed_time_model(track_dt_sub |> filter(track == "AW196499..deploy_id.2023814814."), model_obj = TRUE, void_non_converging = TRUE)
+# model not converged with gamma
+# change to gaussian "fixes"issues
+
+# 
+
+mod_out <- speed_time_model(track_dt |> filter(track == "B175706..deploy_id.2023814800."), model_obj = TRUE, void_non_converging = TRUE)
+# not converged, fit changed to NULL
+# gaussian works 
+
+mod_out <- speed_time_model(track_dt_sub |> filter(track == "B175706..deploy_id.2023814800."), model_obj = TRUE, void_non_converging = TRUE)
+# not converged, fit changed to NULL
+# gaussian not converged either, so no model fitted...
+# note that with date cut this is only 14 days of data...
+
+
+# ~~~~~~~~~~~~~~~~~~
+# SAVANNAH
+# ~~~~~~~~~~~~~~~~~~
+# savanha: "SAV.4360.A" (runSALSA1D error)
+
+set_interactive_app_testing()
+
+track_dt <- rFunction(
+  data = lindesay_dt$savahn,
+  travelcut = 3,
+  create_plots = TRUE,
+  sunrise_leeway = 0,
+  sunset_leeway = 0,
+  altbound = 25,
+  keepAllCols = FALSE
+)
+
+mod_out <- speed_time_model(track_dt |> filter(track == "SAV.4360.A"), model_obj = TRUE, void_non_converging = TRUE)
+# fine to fail, this tag is not on a bird!!
+# sitting in Gabes house
+# fixed by changing to 5 fold CV. uses gamma
+
+# ~~~~~~~~~~~~~~~~~~
+# NAMIBIA SOP
+# ~~~~~~~~~~~~~~~~~~
+# nam_sop: "GA_6594" (runSALSA1D error)
+
+set_interactive_app_testing()
+
+track_dt <- rFunction(
+  data = lindesay_dt$nam_sop,
+  travelcut = 3,
+  create_plots = TRUE,
+  sunrise_leeway = 0,
+  sunset_leeway = 0,
+  altbound = 25,
+  keepAllCols = FALSE
+)
+
+mod_out <- speed_time_model(track_dt |> filter(track == "GA_6594"), model_obj = TRUE, void_non_converging = TRUE)
+# neither model any good - k-fold issue
+# only 14 days of data
+# fixed by changing k-folds to 5. fits using gamma
+
+# ~~~~~~~~~~~~~~~~~~
+# Zambia birds
+# ~~~~~~~~~~~~~~~~~~
+# wb_zam_knd: "A234556..deploy_id.2153340562." (non-convergence); "X163116Z..deploy_id.2388610874" (runSALSA1D error)
+
+set_interactive_app_testing()
+
+track_dt <- rFunction(
+  data = lindesay_dt$wb_zam_knd,
+  travelcut = 3,
+  create_plots = TRUE,
+  sunrise_leeway = 0,
+  sunset_leeway = 0,
+  altbound = 25,
+  keepAllCols = FALSE
+)
+
+mod_out <- speed_time_model(track_dt |> filter(track == "A234556..deploy_id.2153340562."), model_obj = TRUE, void_non_converging = TRUE)
+# gamma issue but fits with log gaussian
+
+mod_out <- speed_time_model(track_dt |> filter(track == "X163116Z..deploy_id.2388610874"), model_obj = TRUE, void_non_converging = TRUE)
+# not enough data
